098830b8dd6efacb7c4fadcbe29aba33e2e48087
JARP <105678913+anderson895@users.noreply.github.com>
2026-02-12 09:06:11 +0800

    udpate

--- backend/app.py
+++ backend/app.py
@@ -85,7 +85,7 @@ def create_app():
     CORS(app, resources={
         r"/api/*": {
             # "origins": ["http://localhost:5173", "http://localhost:3000"],
-            "origins": ["https://lagona.vercel.app"],
+            "origins": ["https://lagona.vercel.app","http://localhost:5173", "http://localhost:3000"],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"],
             "expose_headers": ["Content-Type"],
